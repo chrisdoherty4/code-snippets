a26be5c59b0c47e8c8dccb746b379273bfd58648
Chris Doherty <chris.doherty4@gmail.com>
2018-09-19 22:10:54 +0100

    Fixup cpdrng.c to register device file correctly.

--- linux/driver/rng/cpdrng.c
+++ linux/driver/rng/cpdrng.c
@@ -3,17 +3,13 @@
 #include <linux/device.h>
 #include <linux/kernel.h>
 #include <linux/fs.h>
+#include <linux/cdev.h>
 
 #define DEVICE_NAME "cpdrng"
 #define DRIVER_NAME DEVICE_NAME
 
 #define LOG(level, format, ...) \
   printk(level "[" DRIVER_NAME "] " format, ##__VA_ARGS__)
-
-static int majorNumber;
-static int opens = 0;
-// static struct class *driver_class = NULL;
-// static struct device *driver_device = NULL;
 
 static int device_open(struct inode *, struct file *);
 static int device_release(struct inode *, struct file *);
@@ -27,41 +23,52 @@ static struct file_operations fops = {
   .write = device_write
 };
 
+static struct class *device_class = NULL;
+static dev_t device_number;
+static struct cdev cdev_struct;
+static struct device *device_struct;
+
 static int __init rng_init(void) {
+  int retval = 0;
+
   LOG(KERN_INFO, "initialising");
 
-  majorNumber = register_chrdev(0, DEVICE_NAME, &fops);
+  retval = alloc_chrdev_region(&device_number, 0, 1, DEVICE_NAME);
 
-  if (majorNumber < 0) {
-    LOG(KERN_ALERT, "failed to register character device.\n");
-    return majorNumber;
+  if (retval != 0) {
+    printk(KERN_ERR "Could not allocate character device region");
   }
 
-  LOG(KERN_INFO, "registered character device.\n");
+  device_class = class_create(THIS_MODULE, DEVICE_NAME "_class");
+
+  cdev_init(&cdev_struct, &fops);
+
+  device_struct = device_create(device_class, NULL, device_number, NULL, DEVICE_NAME);
+
+  cdev_add(&cdev_struct, device_number, 1);
 
   return 0;
 }
 
 static void __exit rng_exit(void) {
   LOG(KERN_INFO, "exiting.\n");
-  unregister_chrdev(majorNumber, DEVICE_NAME);
+
+  cdev_del(&cdev_struct);
+
+  device_destroy(device_class, device_number);
+
+  class_destroy(device_class);
+
+  unregister_chrdev_region(device_number, 1);
 }
 
 static int device_open(struct inode *node, struct file *handle)
 {
-  opens++;
-
-  LOG(KERN_INFO, "device opened %d times\n", opens);
-
   return 0;
 }
 
 static int device_release(struct inode *node, struct file *handle)
 {
-  opens--;
-
-  LOG(KERN_INFO, "device closed. %d handles remaining.\n", opens);
-
   return 0;
 }
 
@@ -73,7 +80,7 @@ static ssize_t device_read(struct file *handle, char *data, size_t size, loff_t 
 
 static ssize_t device_write(struct file *handle, const char *data, size_t size, loff_t *offset)
 {
-  LOG(KERN_INFO, "Tried writing to the device.\n");
+  LOG(KERN_INFO, "tried writing to the device.\n");
   return -EINVAL;
 }
 
